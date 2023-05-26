361e5ebb15c758b101376c27188f0ff393dc3d92
Sin_B <66472417+sinbee0402@users.noreply.github.com>
2023-05-26 11:52:52 +0900

    Update hero.dart
    Change getter / setter

--- hero.dart
+++ hero.dart
@@ -2,6 +2,15 @@
 // 05.25.과제
 
 class Hero {
-  int hp;
-  Hero({required this.hp});
+  int _hp;
+
+  int get hp => _hp;
+  set hp(int value) {
+    if (value <= 0) {
+      throw Exception('용사의 hp가 0입니다.');
+    }
+    _hp = value;
+  }
+
+  Hero(this._hp);
 }
