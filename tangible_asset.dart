de4d6c1b42ef8f3c992416a494dd4d0ef9a05231
Sin_B <66472417+sinbee0402@users.noreply.github.com>
2023-05-29 01:30:04 +0900

    Update tangible_asset.dart

--- tangible_asset.dart
+++ tangible_asset.dart
@@ -4,17 +4,18 @@ import 'thing.dart';
 // 05.25.과제
 
 abstract class TangibleAsset extends Asset implements Thing {
-  int price;
-  String color;
-  late final double _weight;
+  final String color;
+  double _weight = 0;
+  //late final double _weight;
+  // 쓰지 않는게 좋다. 이렇게 변수선언만 해도 오류가 안나기 때문이다.
+  // late를 쓰고 생성자에서 초기화를 하지 않는다면 터지기 때문이다.
 
   // 12-1
   TangibleAsset({
     required super.name,
-    required this.price,
+    required super.price,
     required this.color,
-    required double weight,
-  }) : _weight = weight;
+  });
 
   // 12-4
   @override
