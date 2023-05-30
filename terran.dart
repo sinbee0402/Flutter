99e0d0372eae483c2d2628edadfb3326725beb53
Sin_B <66472417+sinbee0402@users.noreply.github.com>
2023-05-30 11:57:43 +0900

    Update terran.dart

--- terran.dart
+++ terran.dart
@@ -1,22 +1,19 @@
 // 05.26.과제
 // 13-4
+import 'zerg.dart';
+
 abstract interface class Terran {}
 
-abstract interface class Bionic {
-  // 생물
+abstract interface class Unit {
   // 13-6
   void unitDesignation(int unit); // 부대 지정 12개까지
 }
 
-abstract interface class Mechanic {} // 기계
+abstract interface class Bionic implements Unit {} // 생물
 
-class Marine extends Medic implements Terran, Bionic {
-  @override
-  void heal() {
-    // Medic 치료
-    super.heal();
-  }
+abstract interface class Mechanic implements Unit {} // 기계
 
+class Marine implements Terran, Bionic {
   @override
   void unitDesignation(int unit) {
     // 부대 지정
@@ -24,9 +21,11 @@ class Marine extends Medic implements Terran, Bionic {
 }
 
 class Medic implements Terran, Bionic {
-  // Terran-Bionic과 SCV, Zerg-Bionic-Unit만 회복
-  void heal() {
-    print('Medic이 해당 개체를 치료하는 중입니다.');
+  // Bionic과 SCV만 회복
+  void heal(Bionic bionic) {
+    if (bionic is! Building) {
+      print('Medic이 해당 개체를 치료하는 중입니다.');
+    }
   }
 
   @override
@@ -35,23 +34,32 @@ class Medic implements Terran, Bionic {
   }
 }
 
-class Tank implements Terran, Mechanic {}
+class Tank implements Terran, Mechanic {
+  @override
+  void unitDesignation(int unit) {
+    // 부대 지정
+  }
+}
 
-class Vulture implements Terran, Mechanic {}
+class Vulture implements Terran, Mechanic {
+  @override
+  void unitDesignation(int unit) {
+    // 부대 지정
+  }
+}
 
-class SCV extends Medic implements Terran, Mechanic {
+class SCV implements Terran, Mechanic, Bionic {
   void gather() {
     // 자원 채취
     print('자원을 채취하는 중입니다.');
   }
 
-  void repair() {
+  void repair(Mechanic mechanic) {
     // Mechanic 수리
   }
 
   @override
-  void heal() {
-    // Medic 치료
-    super.heal();
+  void unitDesignation(int unit) {
+    // 부대 지정
   }
 }
