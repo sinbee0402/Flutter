22941e26c2066dec0104f3af8209180fc77af222
Sin_B <66472417+sinbee0402@users.noreply.github.com>
2023-06-02 01:42:43 +0900

    파일 조작

---
+++ file_copy/file_copy.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+// 06.01.과제
+// 파일 복사
+
+void copy(String source, String target) {
+  final file = File('lib/homework/file_copy/memo.txt');
+  final copyFile = File('lib/homework/file_copy/copy.txt');
+
+  copyFile.writeAsStringSync(file.readAsStringSync());
+  // copyFile.writeAsStringSync(file.readAsStringSync(), mode: FileMode.append);
+}
+
+void main() {
+  try {
+    copy('memo.txt', 'copy.txt');
+  } catch (e) {
+    print('파일 경로가 잘못되었습니다.');
+  }
+}
