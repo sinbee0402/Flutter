d1713d8ad9e983934f14a0037f3a5fc77d33ccac
Sin_B <66472417+sinbee0402@users.noreply.github.com>
2023-06-07 12:15:54 +0900

    Update Trader.dart

--- lamda_function/Trader.dart
+++ lamda_function/Trader.dart
@@ -27,80 +27,68 @@ final transactions = [
 ];
 
 void main() {
-  final list = [];
+  // forEach에서 add를 하는 것보다 addAll로 하는게 더 낫다.
+  // final list = [];
+  // list.addAll(transactions.where((e) => e.year == 2011));
+
   // 1번.
   // 2011년에 일어난 모든 트랜잭션/가격 기준 오름차순/이름 나열
-  transactions.where((e) => e.year == 2011).forEach((e) {
-    list.add(e);
-  });
-  list.sort((a, b) => a.value.compareTo(b.value));
-  final names = list.map((e) => e.trader.name).toList();
-  print(names);
+  // final result1 = transactions
+  //     .where((e) => e.year == 2011)
+  //     .toList()
+  //     .sort((a, b) => a.value.compareTo(b.value));
+  ((transactions.where((e) => e.year == 2011).toList())
+        ..sort((a, b) => a.value.compareTo(b.value)))
+      .map((e) => e.trader.name)
+      .forEach(print);
 
   // 2번.
   // 거래자가 근무하는 모든 도시/중복없이/나열
-  list.clear();
-  transactions.forEach((e) {
-    list.add(e.trader.city);
-  });
-  print(list.toSet().toList());
+  transactions.map((e) => e.trader.city).toSet().forEach(print);
 
   // 3번.
   // 케임브리지에서 근무하는 모든 거래자/이름순 정렬/나열
-  list.clear();
-  transactions.where((e) => e.trader.city == 'Cambridge').forEach((e) {
-    list.add(e.trader.name);
-  });
-  list.sort();
-  print(list);
+  ((transactions.where((e) => e.trader.city == 'Cambridge').toList())
+        ..sort((a, b) => a.trader.name.compareTo(b.trader.name)))
+      .forEach(print);
 
   // 4번.
   // 모든 거래자의 이름/알파벳순 정렬/나열
-  list.clear();
-  transactions.forEach((e) {
-    list.add(e.trader.name);
-  });
-  list.sort();
-  print(list);
+  ((transactions.map((e) => e.trader.name).toList())
+        ..sort((a, b) => a.compareTo(b)))
+      .forEach(print);
 
   // 5번.
   // 밀라노에 거래자가 있는지
-  list.clear();
-  transactions.where((e) => e.trader.city == 'Milan').forEach((e) {
-    list.add(e.trader.name);
-  });
-  print(list);
+  print(transactions.any((e) => e.trader.city == 'Milan'));
 
   // 6번.
   // 케임브리지에 거주하는 거래자/모든 트랙잭션값/출력
-  // 트랜잭션값이 거래값인가..? value값??
-  transactions.where((e) => e.trader.city == 'Cambridge').forEach((e) {
-    print('Transaction 값');
-    print('${e.trader.name}, ${e.trader.city}');
-    print('${e.year}, ${e.value}');
-    print('------------------------');
-  });
+  transactions.where((e) => e.trader.city == 'Cambridge').forEach(print);
 
   // 7번.
   // 전체 트랜잭션 중 최대값
   // list.clear();
   // transactions.map((e) => list.add(e.value));
-  // final maxValue = list.reduce((e, v) => max(e, v)); // 이거 왜 안되는거지..
-  //print(list.reduce((e, v) => e > v ? e : v));  // 얘도 안된다
-  // final maxValue = list.reduce((e, v) => max); // 근데 이건 되네. 뭔차이야..?
-  final maxValue = transactions.fold(
-      0,
-      (previousValue, transaction) => previousValue > transaction.value
-          ? previousValue
-          : transaction.value);
-  print(maxValue);
+  // final maxValue = list.reduce((e, v) => max); // 이거 왜 안되는거지..
+  // print(list.reduce((e, v) => e > v ? e : v));
+  // final maxValue = transactions.fold(
+  //     0,
+  //     (previousValue, transaction) => previousValue > transaction.value
+  //         ? previousValue
+  //         : transaction.value);
+  // print(maxValue);
+  // print(transactions.map((e) => e.value).toList().reduce(max));
+  // print(transactions.map((e) => e.value).reduce((value, element) => value>element? value:element));
+  print(transactions.map((e) => e.value).fold<int>(0, max));
 
   // 8번.
   // 전체 트랜잭션 중 최소값
-  final minValue = transactions.fold(
-      maxValue,
-      (previousValue, transaction) => previousValue < transaction.value
-          ? previousValue
-          : transaction.value);
-  print(minValue);
+  // final minValue = transactions.fold(
+  //     maxValue,
+  //     (previousValue, transaction) => previousValue < transaction.value
+  //         ? previousValue
+  //         : transaction.value);
+  // print(minValue);
+  print(transactions.map((e) => e.value).reduce(min));
 }
