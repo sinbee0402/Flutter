00bb830d83495eb5fcc05045f04a34db258e20c1
Sin_B <66472417+sinbee0402@users.noreply.github.com>
2023-06-09 10:57:39 +0900

    네트워크 통신(Network communication)

---
+++ HTTP/Movie/movie_info.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+
+import 'package:dart_basic/homework/HTTP/Movie/model/MovieDetail.dart';
+import 'package:dart_basic/homework/HTTP/Movie/model/movie.dart';
+import 'package:http/http.dart' as http;
+
+// 06.08.과제
+
+void main() async {
+  // print(await getAllInfo());
+
+  // 1번. 영화 정보
+  final movie = await getMovieInfo();
+  movie.forEach((e) {
+    print('영화 정보');
+    print('제목: ${e.title}');
+    print('줄거리: ${e.overview}\n');
+  });
+
+  // 2번. 영화 상세 정보
+  final info = await getDetailInfo(569094);
+  info.forEach((key, value) {
+    print('$key: $value');
+  });
+}
+
+// 1번. 영화 정보
+Future<List<Movie>> getMovieInfo() async {
+  // error 2. List type <dynamic> -> <Movie>
+
+  final response = await http.get(Uri.parse(
+      'https://api.themoviedb.org/3/movie/upcoming?api_key=a64533e7ece6c72731da47c9c8bc691f&language=ko-KR&page=1'));
+  final allInfo = jsonDecode(response.body);
+  final List<dynamic> results = allInfo['results'];
+  // error 1. Human error: result -> results
+
+  return results.map((e) => Movie.fromJson(e)).toList();
+}
+
+// 2번. 영화 상세 정보
+// 모델에 담지 않았다.. 내일의 누군가 도와줘요 스피드웨건!
+Future<Map<String, dynamic>> getDetailInfo(int movieId) async {
+  final response = await http.get(Uri.parse(
+      'https://api.themoviedb.org/3/movie/$movieId?api_key=a64533e7ece6c72731da47c9c8bc691f&language=ko-KR&page=1'));
+  final Map<String, dynamic> detailInfo = jsonDecode(response.body);
+  return detailInfo;
+}
---
+++ HTTP/Picture/download.dart
@@ -0,0 +1,91 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:http/http.dart' as http;
+
+// 06.08.과제
+
+void main() async {
+  // 4번.
+  // print('다운로드 시작');
+  // Stopwatch stopwatch = Stopwatch()..start();
+
+  // 3번.
+  final file = await saveFile(
+    await downloadImage('https://alimipro.com/favicon.ico'),
+    'icon.ico',
+  );
+
+  // // 4번.
+  // stopwatch.stop();
+  // print('다운로드 끝');
+  // print('===============');
+  // print('소요 시간 : ${stopwatch.elapsed}');
+  // print('용량 : ${await file.length()}bytes');
+  //
+  // // 5번.
+  // await downloadSequence();
+  // await downloadParallel();
+
+  // 6번. test 안된다.
+  await saveFile(
+    await downloadImage('https://alimipro.com/alkjsdhlkfjahskdjfhla.ico'),
+    'icon.ico',
+  );
+}
+
+// 3번.
+// 함수를 끝까지 작성해야 type을 import 할 수 있다.
+Future<Uint8List> downloadImage(String url) async {
+  // 6번. 확인 안된다.
+  try {
+    final response = await http.get(Uri.parse(url));
+    return response.bodyBytes;
+    // bodyBytes: HTTP 응답의 데이터를 바이트 배열 형태로 나타낸다.
+    // 바이트 배열은 메모리에 저장된 이미지, 파일, 텍스트 등 다양한 데이터를 포함할 수 있다.
+  } catch (e) {
+    // 대체 데이터
+    final response = await http.get(Uri.parse(
+        'https://blog.kakaocdn.net/dn/I3uVW/btsfCsG5Lol/NSKMEyEIqYBQSbiFbcskcK/img.png'));
+    return response.bodyBytes;
+  }
+}
+
+// 3번.
+Future<File> saveFile(Uint8List bytes, String fileName) async {
+  final file = File('lib/homework/HTTP/Picture/$fileName');
+  return await file.writeAsBytes(bytes);
+  // writeAsBytes: 바이트 배열 데이터를 파일로 저장하는 메서드
+  // 해당 파일이 생성되거나 기존 파일이 덮어쓰여진다.
+}
+
+// 5번.
+// 순서
+Future<void> downloadSequence() async {
+  Stopwatch stopwatch = Stopwatch()..start();
+  for (int i = 1; i < 4; i++) {
+    await saveFile(
+        await downloadImage('https://alimipro.com/favicon.ico'), 'icon$i.ico');
+  }
+  stopwatch.stop();
+  print('소요 시간 : ${stopwatch.elapsed}');
+}
+
+// 5번.
+// 병렬
+Future<void> downloadParallel() async {
+  final futures = [
+    saveFile(
+        await downloadImage('https://alimipro.com/favicon.ico'), 'icon4.ico'),
+    saveFile(
+        await downloadImage('https://alimipro.com/favicon.ico'), 'icon5.ico'),
+    saveFile(
+        await downloadImage('https://alimipro.com/favicon.ico'), 'icon6.ico'),
+  ]; // wait을 사용하기 위해서는 List안에서 await를 호출하면 안된다..?
+  Stopwatch stopwatch = Stopwatch()..start();
+
+  await Future.wait(futures);
+
+  stopwatch.stop();
+  print('소요 시간 : ${stopwatch.elapsed}');
+}
