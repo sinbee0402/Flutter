fe2659ecadd5023f0ce413ed7b9993d09f815e7b
sinbee0402 <osb3476@gmail.com>
2023-06-13 00:57:17 +0900

    018. int 변수, setState, 주석으로 화면 중간의 숫자 변경하기

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // count라는 변수에 값 0을 담음.
+  int count = 0;
+
   @override
   Widget build(BuildContext context) {
     // 화면에 X 표시
@@ -52,7 +55,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Text(
-          '0',
+          '$count', // 변수의 값을 넣어서 표시
           style: TextStyle(
             color: Colors.red,
             fontSize: 70,
@@ -68,7 +71,13 @@ class _MyHomePageState extends State<MyHomePage> {
         // onPressed : 버튼을 눌렀을 때의 처리를 하는 부분
         // 괄호와 중괄호가 있는 것들은 함수이다.
         onPressed: () {
-          print('클릭');
+          // setState() : 변경된 내용으로 화면을 그려준다
+          // 변경되는 값이 있을 때 꼭 써줘야한다.
+          setState(() {
+            // 클릭을 했을 시 증가하도록
+            // 화면 그리기 / 화면 갱신
+            count++;
+          });
           // 문장이 끝날 때마다 세미콜론을 써야한다.
           // 컴마가 쓰인 곳들은 한줄이다.
           // 함수는 별도의 문장이기에 세미콜론을 쓴다
