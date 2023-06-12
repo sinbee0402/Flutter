e847271e9f560cd6b7bebac1109151f65fa7579f
sinbee0402 <osb3476@gmail.com>
2023-06-12 21:16:23 +0900

    013. StatefulWidget을 사용하여 화면의 뼈대 만들기

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // 화면에 X 표시
+    // return const Placeholder();
+    // 화면의 빈 공간을 나타냄.
+    // 둘의 차이는 X자 표시를 나타내느냐 아니냐의 차이이다.
+    return Container();
+  }
+}
