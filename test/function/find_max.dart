9f8c995808b449f0d46f9110dabd72b22a8e3ec1
Sin_B <66472417+sinbee0402@users.noreply.github.com>
2023-06-13 16:14:06 +0900

    단위 테스트 (Unit Test)

---
+++ test/function/find_max.dart
@@ -0,0 +1,51 @@
+import 'package:dart_basic/homework/test_function.dart';
+import 'package:test/test.dart';
+
+void main() {
+  List<int> list;
+
+  test('findMax 함수 테스트', () {
+    // given
+    list = [2, 5, 1, 9, 3];
+    // when
+    final result = findMax(list);
+    // then
+    expect(result, 9);
+  });
+
+  test('findMax 함수 테스트', () {
+    // given
+    list = [2, 5, 1, 9, 9];
+    // when
+    final result = findMax(list);
+    // then
+    expect(result, 9);
+  });
+
+  test('findMax 함수 테스트', () {
+    // given
+    list = [1, 1, 1, 1, 1];
+    // when
+    final result = findMax(list);
+    // then
+    expect(result, 1);
+  });
+
+  test('findMax 함수 테스트', () {
+    // given
+    list = [1, 1, 1, 1, 1];
+    // when
+    final result = findMax(list);
+    // then
+    expect(result, 9); // error
+  });
+
+  test('findMax 함수 테스트', () {
+    // given
+    list = [-1, -1, 1, -1, 0];
+    // when
+    final result = findMax(list);
+    // then
+    expect(result, 0); // error
+  });
+}
