e203028a2e22fc2d99bd0f04d24ec811753bffd0
sinbee0402 <osb3476@gmail.com>
2023-06-19 23:14:44 +0900

    ADD stop_watch App

---
+++ lib/stop_watch_screen.dart
@@ -0,0 +1,135 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class StopWatchScreen extends StatefulWidget {
+  const StopWatchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StopWatchScreen> createState() => _StopWatchScreenState();
+}
+
+class _StopWatchScreenState extends State<StopWatchScreen> {
+  // 여기서 가장 중요한 Timer
+  Timer? _timer;
+
+  int _time = 0;
+  bool _isRunning = false;
+
+  final List<String> _lapTimes = [];
+
+  void _clickButton() {
+    // true이면 false로, false면 true로 바꾸기
+    _isRunning = !_isRunning;
+
+    if (_isRunning) {
+      _start();
+    } else {
+      _pause();
+    }
+  }
+
+  void _start() {
+    _timer = Timer.periodic(const Duration(milliseconds: 0), (timer) {
+      setState(() {
+        _time++;
+      });
+    });
+  }
+
+  void _pause() {
+    _timer?.cancel();
+  }
+
+  void _reset() {
+    _isRunning = false;
+    _timer?.cancel();
+    _lapTimes.clear();
+    _time = 0;
+  }
+
+  void _recordLapTime(String time) {
+    _lapTimes.insert(0, '${_lapTimes.length + 1} | $time');
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel(); // null이 아닐 때, cancel
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // 몫 구하기 ~/
+    int sec = _time ~/ 100;
+    // padLeft로 비어있는 곳을 0으로 채우기
+    String hundredth = '${_time % 100}'.padLeft(2, '0');
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('스톱워치'),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                '$sec',
+                style: const TextStyle(fontSize: 50),
+              ),
+              Text(
+                hundredth,
+              )
+            ],
+          ),
+          SizedBox(
+            width: 100,
+            height: 400,
+            child: ListView(
+              children:
+                  _lapTimes.map((time) => Center(child: Text(time))).toList(),
+            ),
+          ),
+          const Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FloatingActionButton(
+                backgroundColor: Colors.blueGrey,
+                onPressed: () {
+                  setState(() {
+                    _reset();
+                  });
+                },
+                child: const Icon(Icons.refresh),
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    _clickButton();
+                  });
+                },
+                child: _isRunning
+                    ? const Icon(Icons.pause)
+                    : const Icon(Icons.play_arrow),
+              ),
+              FloatingActionButton(
+                backgroundColor: Colors.blue,
+                onPressed: () {
+                  setState(() {
+                    _recordLapTime('$sec.$hundredth');
+                  });
+                },
+                child: const Icon(Icons.add),
+              ),
+            ],
+          ),
+          const SizedBox(height: 30)
+        ],
+      ),
+    );
+  }
+}
