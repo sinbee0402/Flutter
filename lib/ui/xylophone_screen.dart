1444bd9eeaecd80f69bd4c57ad9b146b325ff08e
sinbee0402 <osb3476@gmail.com>
2023-06-29 10:51:13 +0900

    Error 발생2. 전과 같은 에러 발생. StatelessWidget으로 변경 후 FutureBuilder를 추가했지만, 같은 에러 발생.

--- lib/ui/xylophone_screen.dart
+++ lib/ui/xylophone_screen.dart
@@ -4,22 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:xylophone/ui/xylophone_view_model.dart';
 
-class XylophoneScreen extends StatefulWidget {
+class XylophoneScreen extends StatelessWidget {
   XylophoneScreen({Key? key}) : super(key: key);
 
-  @override
-  State<XylophoneScreen> createState() => _XylophoneScreenState();
-}
-
-class _XylophoneScreenState extends State<XylophoneScreen> {
   final viewModel = XylophoneViewModel();
-
-  @override
-  void initState() {
-    super.initState();
-    Future.microtask(
-        () => viewModel.initSoundPool().then((value) => setState(() {})));
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,46 +17,58 @@ class _XylophoneScreenState extends State<XylophoneScreen> {
       appBar: AppBar(
         title: const Text('실로폰'),
       ),
-      body: viewModel.isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                  child: gunban('도', Colors.red, viewModel.soundIds[0]),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 24.0),
-                  child: gunban('레', Colors.orange, viewModel.soundIds[1]),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 32.0),
-                  child: gunban('미', Colors.yellow, viewModel.soundIds[2]),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 40.0),
-                  child: gunban('파', Colors.lightGreen, viewModel.soundIds[3]),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 48.0),
-                  child: gunban('솔', Colors.green, viewModel.soundIds[4]),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 56.0),
-                  child: gunban('라', Colors.blue, viewModel.soundIds[5]),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 64.0),
-                  child: gunban(
-                      '시', Colors.deepPurpleAccent, viewModel.soundIds[6]),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 72.0),
-                  child: gunban('도', Colors.purple, viewModel.soundIds[7]),
-                ),
-              ],
-            ),
+      body: FutureBuilder<void>(
+          future: viewModel.initSoundPool(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              // 에러 발생
+              // Unable to load asset: "assets/re.wav".
+              // The asset does not exist or has empty data.
+              return const Center(child: Text('알 수 없는 에러'));
+            } else {
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: gunban('도', Colors.red, viewModel.soundIds[0]),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 24.0),
+                    child: gunban('레', Colors.orange, viewModel.soundIds[1]),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 32.0),
+                    child: gunban('미', Colors.yellow, viewModel.soundIds[2]),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 40.0),
+                    child:
+                        gunban('파', Colors.lightGreen, viewModel.soundIds[3]),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 48.0),
+                    child: gunban('솔', Colors.green, viewModel.soundIds[4]),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 56.0),
+                    child: gunban('라', Colors.blue, viewModel.soundIds[5]),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 64.0),
+                    child: gunban(
+                        '시', Colors.deepPurpleAccent, viewModel.soundIds[6]),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 72.0),
+                    child: gunban('도', Colors.purple, viewModel.soundIds[7]),
+                  ),
+                ],
+              );
+            }
+          }),
     );
   }
 
